79144d4bbc21dca12d26a901fa1a20365c360969
ColtonKor <ckorhummel@csumb.edu>
2024-09-18 18:05:40 -0700

    I put the API into the word of the day page
    I moved wordApi.js into the app file. I then connected it into the index.tsx file so it shows the words on the page. The buttons also randomize the words all that is needed is to add the database now.

--- CST438Project1/app/wordsApi.js
+++ CST438Project1/app/wordsApi.js
@@ -1,23 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
 import { Text, View } from 'react-native';
 
-const WordApi = () => {
+const WordApi = forwardRef((props, ref) => {
   const [words, setWords] = useState(null);
   const [randomWords, setRandomWords] = useState(null);
 
-  // To generate the random word.
   const getRandomWord = async () => {
     try {
       const response = await fetch('https://random-word-api.herokuapp.com/word');
       const json = await response.json();
-      setRandomWords(json[0]); // Assuming json[0] is the word
+      setRandomWords(json[0]);
       console.log(json);
     } catch (error) {
       console.error(error);
     }
   };
 
-  // To get the random word's definition.
   const getWords = async (word) => {
     try {
       const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
@@ -30,11 +28,7 @@ const WordApi = () => {
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      await getRandomWord();
-    };
-
-    fetchData();
+    getRandomWord();
   }, []);
 
   useEffect(() => {
@@ -43,20 +37,22 @@ const WordApi = () => {
     }
   }, [randomWords]);
 
-  // Performing checks to see if the random word or the definition are generated.
+  useImperativeHandle(ref, () => ({
+    fetchNewWord: getRandomWord,
+  }));
+
   if (!randomWords) {
     return <Text>Please try loading a new random word</Text>;
   } else if (!words) {
     return <Text>There is no available definition for this word.</Text>;
   }
 
-  // Will return the random word and the definition.
   return (
     <View>
       <Text>Random Word: {randomWords}</Text>
       <Text>Definition: {words[0]?.meanings[0]?.definitions[0]?.definition || 'No definition available'}</Text>
     </View>
   );
-};
+});
 
 export default WordApi;
